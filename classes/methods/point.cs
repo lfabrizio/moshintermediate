10907f5bb61beca9b8c681e7a23f7bf7ca785e22
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-02-29 15:44:43 -0500

    methods

---
+++ classes/methods/point.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var point = new Point(10, 20);
+            point.Move(new Point(40, 60));
+            Console.WriteLine("Point is sat ({0}, {1}", point.X, point.Y);
+
+            point.Move(100, 200);
+            Console.WriteLine("Point is at ({0}, {1})", point.X, point.Y);
+        }
+    }
+}
