f7eb63966f70ea33556063ebfa3627488a789d60
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-02-29 13:18:41 -0500

    classes

---
+++ classes/customer.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpIntermediate
+{
+        public class Customer
+        {
+            public int Id;
+            public string Name;
+            public List<Order> Orders;
+
+        public Customer()
+        {
+            Orders = new List<Order>();
+        }
+        public Customer(int id)
+            : this()
+        {
+            this.Id = id;
+
+        }
+
+        public Customer(int id, string name)
+            : this(id)
+        {
+            this.Name = name;
+        }
+        }
+    }
+
