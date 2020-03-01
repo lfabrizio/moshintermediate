a15bb05e4d685c79e51d13bed9c256bd5dacc169
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-03-01 16:53:56 -0500

    properties

---
+++ classes/properties/person.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace Properties
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Username { get; set; }
+        public Person(DateTime birthdate)
+        {
+            Birthdate = birthdate;
+        }
+        public DateTime Birthdate { get; private set; }
+
+        public int Age
+        {
+            get
+            {
+                var timeSpan = DateTime.Today - Birthdate;
+                var years = timeSpan.Days/365;
+
+                return years;
+            }
+        }
+    }
+}
--- classes/properties/program.cs
+++ classes/properties/program.cs
@@ -6,26 +6,12 @@ using System.Threading.Tasks;
 
 namespace Properties
 {
-    public class Person
-    {
-        public DateTime Birthdate { get; set; }
-
-        public int Age
-        {
-            get
-            { 
-                var timeSpan = DateTime.Today - Birthdate;
-                var years = timeSpan.Days / 365;
-
-                return years;
-            }
-        }
-    }
     class Program
     {
-       
         static void Main(string[] args)
         {
+            var person = new Person(new DateTime(1982, 1, 1));
+            Console.WriteLine(person.Age)
         }
     }
 }
