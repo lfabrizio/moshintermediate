113b779a91a3f4ed7c60f9c7f04c292295059354
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-03-04 21:06:26 -0500

    inheritance

--- association/classcoupling.cs
+++ association/classcoupling.cs
@@ -1,1 +1,21 @@
 // a measure of how interconnected classes and subsytems are 
+using System;
+
+namespace Inheritance
+{
+    public class PresentationObject
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public void Copy()
+        {
+            Console.WriteLine("Object Copy");
+        }
+
+        public void Duplicate()
+        {
+            Console.WriteLine("Object was duplicated");
+        }
+    }
+}
